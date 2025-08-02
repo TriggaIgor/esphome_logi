071863c65c2ae432edeca754115885938e730577
TriggaIgor <78341534+TriggaIgor@users.noreply.github.com>
2025-08-02 22:28:41 +0400

    Update switch.py

--- esphome/components/mouse/switch.py
+++ esphome/components/mouse/switch.py
@@ -14,7 +14,7 @@ CONF_MAX_SPEED = "max_speed"
 CONF_ACCELERATION_RATE = "acceleration_rate"
 CONF_DECELERATION_RATE = "deceleration_rate"
 
-CONFIG_SCHEMA = switch.SWITCH_SCHEMA.extend({
+CONFIG_SCHEMA = switch.switch_schema.extend({
     cv.GenerateID(): cv.declare_id(Mouse),
     cv.Optional(CONF_BASE_SPEED, default=8.0): cv.float_range(min=0.1, max=100.0),
     cv.Optional(CONF_JITTER_AMOUNT, default=1.5): cv.float_range(min=0.0, max=10.0),
